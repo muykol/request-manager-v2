219336702d4fe950d8e59f257498bfacbfc53c58
muykol <olumuyiwa.kolayemi@slalom.com>
2024-02-01 17:19:55 -0500

    update code to fix error and cleanup

--- __main__.py
+++ __main__.py
@@ -41,11 +41,11 @@ for index, policy_arn in enumerate(policy_arns):
 #     role=lambda_role.name,
 #     policy_arn='arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'
 # )
-
+####===== Notification Manager Code ==================#######
 # Define an SNS topic
 sns_topic = aws.sns.Topic('EventTopic')
 
-# Lambda functions
+# Define notification manager Lambda functions
 lambda_function = aws.lambda_.Function('notification-manager',
     code=pulumi.AssetArchive({".": pulumi.FileArchive("./notification_manager")}),
     role=lambda_role.arn,
@@ -53,6 +53,23 @@ lambda_function = aws.lambda_.Function('notification-manager',
     runtime='python3.10'
 )
 
+# Give the notification Lambda permission to be invoked by the SNS topic
+lambda_permission = aws.lambda_.Permission(f'{project_name}-LambdaPermission',
+    action='lambda:InvokeFunction',
+    function=lambda_function.name,
+    principal='sns.amazonaws.com',  
+    source_arn=sns_topic.arn
+)
+
+# Subscribe the notification manager Lambda function to the SNS topic
+snsLambda_subscription = aws.sns.TopicSubscription('snsLambda-Subscription',
+    topic=sns_topic.arn,
+    protocol='lambda',
+    endpoint=lambda_function.arn
+)
+
+####===== Contract Processor Code ==================#######
+# Define Contract Processor Lambda functions that have EventTopic arn as its environmental variable
 lambda_function = aws.lambda_.Function('contract_processor',
     code=pulumi.AssetArchive({".": pulumi.FileArchive("./contract_processor")}),
     role=lambda_role.arn,
@@ -65,20 +82,7 @@ lambda_function = aws.lambda_.Function('contract_processor',
     )
 )
 
-# Give the Lambda permission to be invoked by the SNS topic
-lambda_permission = aws.lambda_.Permission(f'{project_name}-LambdaPermission',
-    action='lambda:InvokeFunction',
-    function=lambda_function.name,
-    principal='sns.amazonaws.com',  
-    source_arn=sns_topic.arn
-)
-
-# Subscribe the Lambda function to the SNS topic
-snsLambda_subscription = aws.sns.TopicSubscription('snsLambda-Subscription',
-    topic=sns_topic.arn,
-    protocol='lambda',
-    endpoint=lambda_function.arn
-)
+####===== Request Manager Queue Code ==================#######
 
 # Create an SQS queue
 sqs_queue = aws.sqs.Queue('request_manager_queue')
@@ -97,6 +101,8 @@ request_policy_document = sqs_queue.arn.apply(lambda arn: aws.iam.get_policy_doc
         values=[sns_topic.arn],
     )],
 )]))
+
+# Create an SQS queue policy to allow the SNS topic to publish to the SQS queue
 request_queue_policy = aws.sqs.QueuePolicy("requestQueuePolicy",
     queue_url=sqs_queue.id,
     policy=request_policy_document.json)
@@ -107,26 +113,6 @@ snsSqs_subscription = aws.sns.TopicSubscription('snsSqs-Subscription',
     endpoint=sqs_queue.arn
 )
 
-
-# # Create an SQS queue policy to allow the SNS topic to publish to the SQS queue
-# queue_policy = sqs_queue.arn.apply(lambda arn: json.dumps({
-#         "Version": "2012-10-17",
-#         "Statement": [{
-#             "Effect": "Allow",
-#             "Principal": {"Service": "sns.amazonaws.com"},
-#             "Action": "sqs:SendMessage",
-#             "Resource": arn,
-#             "Condition": {
-#                 "ArnEquals": {"aws:SourceArn": sns_topic.arn}
-#             }
-#         }]
-#     }))
-# queue_policy_resource = aws.sqs.QueuePolicy(
-#     'request-manager-queue-policy',
-#     queue_url=sqs_queue.id,
-#     policy=queue_policy # Here queue_policy is already a JSON string, not an Output object
-# )
-
 # SSM Parameter to store the SNS topic URL
 sns_topic_url_param = aws.ssm.Parameter('EventTopicArn',
     name='EventTopicArn',  # Name of the parameter
@@ -134,14 +120,14 @@ sns_topic_url_param = aws.ssm.Parameter('EventTopicArn',
     value=sns_topic.arn  # The actual value to store, here ARN is used as the URL identifier for the SNS Topic
 )
 
-# SSM Parameter to store the Lambda function name
+# SSM Parameter to store the contract processor Lambda function name
 lambda_function_name_param = aws.ssm.Parameter('contractProcessorName',
     name='contractProcessor',    # Name of the parameter
     type='String',               # Type of the parameter (String, StringList, SecureString)
     value=lambda_function.name   # The actual value to store, which is the name of the Lambda Function
 )
 
-# Store the SQS queue URL in the AWS SSM Parameter Store
+# SSM Parameter to store the SQS queue URL
 ssm_parameter = aws.ssm.Parameter('request_manager_queue_url',
     name='request_manager_queue',
     type='String',
